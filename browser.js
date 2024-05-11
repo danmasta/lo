4902a2f44f666d88e76b687430065315e5dfe8ae
Daniel Smith <dannmasta@gmail.com>
2024-05-10 20:43:39 -0700

    add browser entrypoint

---
+++ browser.js
@@ -0,0 +1,21 @@
+import { BREAK, noop, addTypes, hasOwn } from './lib/constants.js';
+import * as types from './lib/types.js';
+import * as util from './lib/util.js';
+import typesBrowser from './types/browser.js';
+import * as libBrowser from './lib/browser.js';
+
+addTypes(typesBrowser);
+
+export { BREAK, noop, hasOwn } from './lib/constants.js';
+export * from './lib/types.js';
+export * from './lib/util.js';
+export * from './lib/browser.js';
+
+export default {
+    BREAK,
+    noop,
+    hasOwn,
+    ...types,
+    ...util,
+    ...libBrowser
+};
