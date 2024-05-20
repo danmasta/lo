cb5792bba0c61247d8907bbeb4f685bc05c56374
Daniel Smith <dannmasta@gmail.com>
2024-05-20 15:19:44 -0700

    add tests for tap, some, every, filter, remove

--- tests/iterate.js
+++ tests/iterate.js
@@ -34,4 +34,74 @@ describe('Iterate', () => {
         expect(res2).to.eql([1,3]);
     });
 
+    it('tap', () => {
+        let res = lo.tap(fx.arr1, val => {
+            return val*2;
+        });
+        expect(res).to.eql([1,2,3]);
+    });
+
+    it('tapNotNil', () => {
+        let res = lo.tapNotNil(fx.arr2, val => {
+            return val*2;
+        });
+        expect(res).to.eql([1,2,3]);
+    });
+
+    it('some', () => {
+        let res = lo.some(fx.arr1, val => {
+            return val % 2 === 0 ? true: false;
+        });
+        expect(res).to.be.true;
+    });
+
+    it('someNotNil', () => {
+        let res = lo.someNotNil(fx.arr2, val => {
+            return val % 2 === 0 ? true: false;
+        });
+        expect(res).to.be.true;
+    });
+
+    it('every', () => {
+        let res = lo.every(fx.arr1, val => {
+            return val % 2 === 0 ? true: false;
+        });
+        expect(res).to.be.false;
+    });
+
+    it('everyNotNil', () => {
+        let res = lo.everyNotNil(fx.arr2, val => {
+            return val % 2 === 0 ? true: false;
+        });
+        expect(res).to.be.false;
+    });
+
+    it('filter', () => {
+        let res = lo.filter(fx.arr1, val => {
+            return val % 2 === 0 ? true : false;
+        });
+        expect(res).to.eql([2]);
+    });
+
+    it('filterNotNil', () => {
+        let res = lo.filterNotNil(fx.arr1, val => {
+            return val % 2 === 0 ? true : false;
+        });
+        expect(res).to.eql([2]);
+    });
+
+    it('remove', () => {
+        let res = lo.remove(fx.arr1, val => {
+            return val % 2 === 0 ? true : false;
+        });
+        expect(res).to.eql([1,3]);
+    });
+
+    it('removeNotNil', () => {
+        let res = lo.removeNotNil(fx.arr1, val => {
+            return val % 2 === 0 ? true : false;
+        });
+        expect(res).to.eql([1,3]);
+    });
+
 });
