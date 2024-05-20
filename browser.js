7a0df45d3d600203beed2195e16ea88474108fdc
Daniel Smith <dannmasta@gmail.com>
2024-05-20 16:09:01 -0700

    move iteration functions to separate file

--- browser.js
+++ browser.js
@@ -1,5 +1,6 @@
 import { BREAK, noop, addTypes } from './lib/constants.js';
 import * as types from './lib/types.js';
+import * as iterate from './lib/iterate.js';
 import * as util from './lib/util.js';
 import typesBrowser from './types/browser.js';
 import * as libBrowser from './lib/browser.js';
@@ -8,6 +9,7 @@ addTypes(typesBrowser);
 
 export { BREAK, noop } from './lib/constants.js';
 export * from './lib/types.js';
+export * from './lib/iterate.js';
 export * from './lib/util.js';
 export * from './lib/browser.js';
 
@@ -15,6 +17,7 @@ export default {
     BREAK,
     noop,
     ...types,
+    ...iterate,
     ...util,
     ...libBrowser
 };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { BREAK, noop, addTypes } from './lib/constants.js';
 import * as types from './lib/types.js';
+import * as iterate from './lib/iterate.js';
 import * as util from './lib/util.js';
 import typesNode from './types/node.js';
 import * as libNode from './lib/node.js';
@@ -8,6 +9,7 @@ addTypes(typesNode);
 
 export { BREAK, noop } from './lib/constants.js';
 export * from './lib/types.js';
+export * from './lib/iterate.js';
 export * from './lib/util.js';
 export * from './lib/node.js';
 
@@ -15,6 +17,7 @@ export default {
     BREAK,
     noop,
     ...types,
+    ...iterate,
     ...util,
     ...libNode
 };
