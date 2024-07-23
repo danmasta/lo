9b3aae026a8bea730f3e2d4a9087d2f2da75e746
Daniel Smith <dannmasta@gmail.com>
2024-07-22 20:46:24 -0700

    add tests for env, argv

---
+++ tests/node.js
@@ -0,0 +1,36 @@
+describe('Node', () => {
+
+    it('env', () => {
+        expect(lo.env('TEST1')).to.be.undefined;
+        lo.env('TEST2', 100);
+        expect(lo.env('TEST2')).to.equal('100');
+        lo.env('TEST3', null);
+        expect(lo.env('TEST3')).to.be.undefined;
+        lo.env('TEST3', 'null');
+        expect(lo.env('TEST3')).to.be.null;
+        lo.env('TEST3', 100);
+        expect(lo.env('TEST3')).to.equal('100');
+    });
+
+    it('argv', () => {
+        let args = lo.argv('-abc -d=1 -e 1 -f100 --no-g - --test=1 --test 2 one two -- three four --test=fizz= --some-dir=/tmp');
+        expect(args).to.eql({
+            _pos: ['one', 'two'],
+            a: true,
+            b: true,
+            c: true,
+            d: 1,
+            e: 1,
+            f: 100,
+            g: false,
+            '-': true,
+            test: [1, 2],
+            _sub: {
+                _pos: ['three', 'four'],
+                test: 'fizz=',
+                someDir: '/tmp'
+            }
+        });
+    });
+
+});
