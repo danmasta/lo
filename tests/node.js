48f6302d323053938f31f65220f126631124ae3a
Daniel Smith <dannmasta@gmail.com>
2024-07-24 15:57:04 -0700

    add tests for resolve fns

--- tests/node.js
+++ tests/node.js
@@ -1,16 +1,7 @@
-describe('Node', () => {
+import { homedir } from 'node:os';
+import { resolve } from 'node:path';
 
-    it('env', () => {
-        expect(lo.env('TEST1')).to.be.undefined;
-        lo.env('TEST2', 100);
-        expect(lo.env('TEST2')).to.equal(100);
-        lo.env('TEST3', null);
-        expect(lo.env('TEST3')).to.be.null;
-        lo.env('TEST3', undefined);
-        expect(lo.env('TEST3')).to.be.undefined;
-        lo.env('TEST3', 100);
-        expect(lo.env('TEST3')).to.equal(100);
-    });
+describe('Node', () => {
 
     it('argv', () => {
         let args = lo.argv('-abc -d=1 -e 1 -f100 --no-g - --test=1 --test 2 one two -- three four --test=fizz= --some-dir=/tmp');
@@ -33,6 +24,18 @@ describe('Node', () => {
         });
     });
 
+    it('env', () => {
+        expect(lo.env('TEST1')).to.be.undefined;
+        lo.env('TEST2', 100);
+        expect(lo.env('TEST2')).to.equal(100);
+        lo.env('TEST3', null);
+        expect(lo.env('TEST3')).to.be.null;
+        lo.env('TEST3', undefined);
+        expect(lo.env('TEST3')).to.be.undefined;
+        lo.env('TEST3', 100);
+        expect(lo.env('TEST3')).to.equal(100);
+    });
+
     it('optsFromArgv', () => {
         let args = '-e true -d false --enabled false --test 100 -';
         let opts = {
@@ -41,7 +44,7 @@ describe('Node', () => {
             test: undefined,
             stdin: '-',
             dir: null
-        }
+        };
         expect(lo.optsFromArgv(opts, { args })).to.eql({
             enabled: true,
             disabled: false,
@@ -49,6 +52,26 @@ describe('Node', () => {
             stdin: true,
             dir: undefined
         });
+    });
+
+    it('resolve', () => {
+        expect(lo.resolve('~')).to.equal(homedir());
+        expect(lo.resolve('./package.json')).to.equal(resolve('./package.json'));
+        expect(lo.resolve('./_setup.js', './lib')).to.equal(resolve('./lib/_setup.js'));
+    });
+
+    it('resolveIfExists', async () => {
+        let pkg = resolve('./package.json');
+        expect(await lo.resolveIfExists('./package.json')).to.equal(pkg);
+        expect(await lo.resolveIfExists('./package', { exts: '.json' })).to.equal(pkg);
+        expect(await lo.resolveIfExists('./package', { require: true })).to.equal(pkg);
+    });
+
+    it('resolveIfExistsSync', () => {
+        let pkg = resolve('./package.json');
+        expect(lo.resolveIfExistsSync('./package.json')).to.equal(pkg);
+        expect(lo.resolveIfExistsSync('./package', { exts: '.json' })).to.equal(pkg);
+        expect(lo.resolveIfExistsSync('./package', { require: true })).to.equal(pkg);
     });
 
 });
