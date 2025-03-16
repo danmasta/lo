b923e174852d71360b7c9366ab76dd6b3460f392
Daniel Smith <dannmasta@gmail.com>
2025-03-16 14:16:24 -0700

    update qjs entrypoint

--- qjs.js
+++ qjs.js
@@ -1,16 +1,21 @@
 import * as argv from './lib/argv.js';
-import { BREAK, noop, TYPES } from './lib/constants.js';
+import { addTypes, BREAK, noop, TYPES } from './lib/constants.js';
 import * as env from './lib/env.js';
 import * as ip from './lib/ip.js';
 import * as iterate from './lib/iterate.js';
+import * as lib from './lib/node.js';
 import * as types from './lib/types.js';
 import * as util from './lib/util.js';
+import supplemental from './types/node.js';
+
+addTypes(supplemental);
 
 export * from './lib/argv.js';
 export { BREAK, noop, TYPES } from './lib/constants.js';
 export * from './lib/env.js';
 export * from './lib/ip.js';
 export * from './lib/iterate.js';
+export * from './lib/node.js';
 export * from './lib/types.js';
 export * from './lib/util.js';
 
@@ -22,6 +27,7 @@ export default {
     ...env,
     ...ip,
     ...iterate,
+    ...lib,
     ...types,
     ...util
 };
