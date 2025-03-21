ca45da417418af885eb3ef8855ff59b60024bbc1
Daniel Smith <dannmasta@gmail.com>
2025-03-21 16:30:16 -0700

    update argv tests

--- tests/node.js
+++ tests/node.js
@@ -1,12 +1,12 @@
 import { homedir } from 'node:os';
 import { resolve } from 'node:path';
-import { argv } from '../lib/argv.js';
+import { optsFromArgv, parseArgv } from '../lib/argv.js';
 
 describe('Node', () => {
 
     it('argv', () => {
-        let args = argv('-abc -d=1 -e 1 -f100 --no-g - --test=1 --test 2 one two -- three four --test=fizz= --some-dir=/tmp');
-        expect(args).to.eql({
+        let argv = parseArgv('-abc -d=1 -e 1 -f100 --no-g - --test=1 --test 2 one two -- three four --test=fizz= --some-dir=/tmp');
+        expect(argv).to.eql({
             _: ['one', 'two'],
             a: true,
             b: true,
@@ -23,18 +23,18 @@ describe('Node', () => {
                 'some-dir': '/tmp'
             }
         });
-        expect(argv(`--test="space str" --param="test str"`)).to.eql({
+        expect(parseArgv(`--test="space str" --param="test str"`)).to.eql({
             _: [],
             test: 'space str',
             param: 'test str'
         });
-        expect(argv(`-t 'space str' -p='test str'`)).to.eql({
+        expect(parseArgv(`-t 'space str' -p='test str'`)).to.eql({
             _: [],
             t: 'space str',
             p: 'test str'
         });
         // Note: mixing case isn't supported
-        expect(argv(`-t "space str" -p='test str'`)).to.eql({
+        expect(parseArgv(`-t "space str" -p='test str'`)).to.eql({
             _: ["str'"],
             t: 'space str',
             p: "'test"
@@ -54,7 +54,7 @@ describe('Node', () => {
     });
 
     it('optsFromArgv', () => {
-        let args = '-e true -d false --enabled false --test 100 -';
+        let argv = '-e true -d false --enabled false --test 100 -';
         let opts = {
             enabled: 'e',
             disabled: 'd',
@@ -62,7 +62,7 @@ describe('Node', () => {
             stdin: '-',
             dir: null
         };
-        expect(lo.optsFromArgv(opts, { args })).to.eql({
+        expect(optsFromArgv(opts, { argv })).to.eql({
             enabled: true,
             disabled: false,
             test: 100,
