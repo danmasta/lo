d38d033632924615b7d881d8a580a831e02e95a2
Daniel Smith <dannmasta@gmail.com>
2025-06-01 22:09:53 -0700

    add eol export

--- polyfill/qjs/os.js
+++ polyfill/qjs/os.js
@@ -1,9 +1,15 @@
+import { EOL } from './core.js';
 import { env } from './process.js';
 
 export function homedir () {
     return env.HOME;
 }
 
+export {
+    EOL
+};
+
 export default {
-    homedir
+    homedir,
+    EOL
 };
