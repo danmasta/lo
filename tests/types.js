c498fdd5679ba5db7b45758347810b94ba69c1ad
Daniel Smith <d@danmasta.net>
2026-08-08 10:34:36 -0700

    add tests for node, extended spec types

--- tests/types.js
+++ tests/types.js
@@ -1,61 +1,75 @@
+import { EventEmitter } from 'node:events';
 import { env } from 'node:process';
+import { StringDecoder } from 'node:string_decoder';
 import { runInNewContext } from 'node:vm';
 import { TYPES } from '../lib/constants.js';
-import { getType, of } from '../lib/types.js';
+import {
+    getCtorType,
+    getType,
+    hasForEach,
+    isCollection,
+    isError,
+    of,
+    toArray,
+    toString,
+    toType
+} from '../lib/types.js';
 
 describe('Types', () => {
 
     it('get type by instance', async () => {
         // Instances should be type of the constructor
-        expect(lo.getType({})).to.equal(TYPES.Object);
-        expect(lo.getType([])).to.equal(TYPES.Array);
-        expect(lo.getType(Object.create(Number.prototype))).to.equal(TYPES.Number);
-        expect(lo.getType(Object.create(null))).to.equal(TYPES.Object);
-        expect(lo.getType(Object.create({}))).to.equal(TYPES.Object);
-        expect(lo.getType(123)).to.equal(TYPES.Number);
-        expect(lo.getType(new Number(123))).to.equal(TYPES.Number);
-        expect(lo.getType(true)).to.equal(TYPES.Boolean);
-        expect(lo.getType(new Date)).to.equal(TYPES.Date);
-        expect(lo.getType(NaN)).to.equal(TYPES.NaN);
-        expect(lo.getType(null)).to.equal(TYPES.Null);
-        expect(lo.getType(undefined)).to.equal(TYPES.Undefined);
-        expect(lo.getType(fx.arrow)).to.equal(TYPES.Function);
-        expect(lo.getType(fx.fn)).to.equal(TYPES.Function);
-        expect(lo.getType(fx.promise)).to.equal(TYPES.Promise);
-        expect(lo.getType(new fx.TestClass)).to.equal(TYPES.TestClass);
-        expect(lo.getType(new fx.TestSubClass)).to.equal(TYPES.TestSubClass);
-        expect(lo.getType(new fx.TestError)).to.equal(TYPES.TestError);
-        expect(lo.getType(new fx.TestSubError)).to.equal(TYPES.TestSubError);
-        expect(lo.getType(new fx.TestArray)).to.equal(TYPES.Array);
-        expect(lo.getType(fx.readable)).to.equal(TYPES.Readable);
-        expect(lo.getType(fx.buff)).to.equal(TYPES.Buffer);
-        expect(lo.getType(await import('../lib/types.js'))).to.equal(TYPES.Module);
-        expect(lo.getType(env)).to.equal(TYPES.Env);
+        expect(getType({})).to.equal(TYPES.Object);
+        expect(getType([])).to.equal(TYPES.Array);
+        expect(getType(Object.create(Number.prototype))).to.equal(TYPES.Number);
+        expect(getType(Object.create(null))).to.equal(TYPES.Object);
+        expect(getType(Object.create({}))).to.equal(TYPES.Object);
+        expect(getType(123)).to.equal(TYPES.Number);
+        expect(getType(new Number(123))).to.equal(TYPES.Number);
+        expect(getType(true)).to.equal(TYPES.Boolean);
+        expect(getType(new Date)).to.equal(TYPES.Date);
+        expect(getType(NaN)).to.equal(TYPES.NaN);
+        expect(getType(null)).to.equal(TYPES.Null);
+        expect(getType(undefined)).to.equal(TYPES.Undefined);
+        expect(getType(fx.arrow)).to.equal(TYPES.Function);
+        expect(getType(fx.fn)).to.equal(TYPES.Function);
+        expect(getType(fx.promise)).to.equal(TYPES.Promise);
+        expect(getType(new fx.TestClass)).to.equal(TYPES.TestClass);
+        expect(getType(new fx.TestSubClass)).to.equal(TYPES.TestSubClass);
+        expect(getType(new fx.TestError)).to.equal(TYPES.TestError);
+        expect(getType(new fx.TestSubError)).to.equal(TYPES.TestSubError);
+        expect(getType(new fx.TestArray)).to.equal(TYPES.Array);
+        expect(getType(fx.readable)).to.equal(TYPES.Readable);
+        expect(getType(fx.buff)).to.equal(TYPES.Buffer);
+        expect(getType(new EventEmitter)).to.equal(TYPES.EventEmitter);
+        expect(getType(new StringDecoder)).to.equal(TYPES.StringDecoder);
+        expect(getType(await import('../lib/types.js'))).to.equal(TYPES.Module);
+        expect(getType(env)).to.equal(TYPES.Env);
         // Constructor functions should be type of Function
-        expect(lo.getType(Promise)).to.equal(TYPES.Function);
-        expect(lo.getType(Boolean)).to.equal(TYPES.Function);
-        expect(lo.getType(fx.TestClass)).to.equal(TYPES.Function);
-        expect(lo.getType(fx.TestError)).to.equal(TYPES.Function);
-        expect(lo.getType(fx.TestArray)).to.equal(TYPES.Function);
+        expect(getType(Promise)).to.equal(TYPES.Function);
+        expect(getType(Boolean)).to.equal(TYPES.Function);
+        expect(getType(fx.TestClass)).to.equal(TYPES.Function);
+        expect(getType(fx.TestError)).to.equal(TYPES.Function);
+        expect(getType(fx.TestArray)).to.equal(TYPES.Function);
         // Accept type objects
         expect(getType(TYPES.Array)).to.equal(TYPES.Array);
         expect(getType(TYPES.Set)).to.equal(TYPES.Set);
     });
 
     it('get type by constructor', () => {
-        expect(lo.getCtorType(Promise)).to.equal(TYPES.Promise);
-        expect(lo.getCtorType(Boolean)).to.equal(TYPES.Boolean);
-        expect(lo.getCtorType(Date)).to.equal(TYPES.Date);
-        expect(lo.getCtorType(NaN)).to.equal(TYPES.NaN);
-        expect(lo.getCtorType(null)).to.equal(TYPES.Null);
-        expect(lo.getCtorType(undefined)).to.equal(TYPES.Undefined);
-        expect(lo.getCtorType(Error)).to.equal(TYPES.Error);
-        expect(lo.getCtorType(fx.TestClass)).to.equal(TYPES.TestClass);
-        expect(lo.getCtorType(fx.TestSubClass)).to.equal(TYPES.TestSubClass);
-        expect(lo.getCtorType(fx.TestError)).to.equal(TYPES.TestError);
-        expect(lo.getCtorType(fx.TestSubError)).to.equal(TYPES.TestSubError);
-        expect(lo.getCtorType(fx.TestArray)).to.equal(TYPES.TestArray);
-        expect(lo.getCtorType(fx.Readable)).to.equal(TYPES.Readable);
+        expect(getCtorType(Promise)).to.equal(TYPES.Promise);
+        expect(getCtorType(Boolean)).to.equal(TYPES.Boolean);
+        expect(getCtorType(Date)).to.equal(TYPES.Date);
+        expect(getCtorType(NaN)).to.equal(TYPES.NaN);
+        expect(getCtorType(null)).to.equal(TYPES.Null);
+        expect(getCtorType(undefined)).to.equal(TYPES.Undefined);
+        expect(getCtorType(Error)).to.equal(TYPES.Error);
+        expect(getCtorType(fx.TestClass)).to.equal(TYPES.TestClass);
+        expect(getCtorType(fx.TestSubClass)).to.equal(TYPES.TestSubClass);
+        expect(getCtorType(fx.TestError)).to.equal(TYPES.TestError);
+        expect(getCtorType(fx.TestSubError)).to.equal(TYPES.TestSubError);
+        expect(getCtorType(fx.TestArray)).to.equal(TYPES.TestArray);
+        expect(getCtorType(fx.Readable)).to.equal(TYPES.Readable);
     });
 
     it('get type for exotic callables', () => {
@@ -73,46 +87,72 @@ describe('Types', () => {
         expect(getType(new Fn)).to.not.be.undefined;
     });
 
+    it('get type for extended spec types', () => {
+        // Native error subtypes
+        expect(getType(new TypeError)).to.equal(TYPES.TypeError);
+        expect(getType(new RangeError)).to.equal(TYPES.RangeError);
+        expect(getType(new SyntaxError)).to.equal(TYPES.SyntaxError);
+        expect(getType(new AggregateError([]))).to.equal(TYPES.AggregateError);
+        expect(isError(new TypeError)).to.be.true;
+        // Binary and iterator additions
+        expect(getType(new Float16Array(1))).to.equal(TYPES.Float16Array);
+        expect(getType(''.matchAll(/(?:)/g))).to.equal(TYPES['RegExp String Iterator']);
+        // Web platform types
+        expect(getType(new TextEncoder)).to.equal(TYPES.TextEncoder);
+        expect(getType(new AbortController)).to.equal(TYPES.AbortController);
+        expect(getType(AbortSignal.abort())).to.equal(TYPES.AbortSignal);
+        expect(getType(new Event('x'))).to.equal(TYPES.Event);
+        expect(getType(new CustomEvent('x'))).to.equal(TYPES.CustomEvent);
+        expect(getType(new DOMException)).to.equal(TYPES.DOMException);
+        // MessagePort inherits EventTarget's toStringTag, but the constructor still resolves
+        expect(getType(new MessageChannel().port1)).to.equal(TYPES.EventTarget);
+        expect(getCtorType(MessagePort)).to.equal(TYPES.MessagePort);
+        // Iterable collections
+        expect(isCollection(new Float16Array(1))).to.be.true;
+        expect(hasForEach(new Float16Array(1))).to.be.true;
+        expect(isCollection(''.matchAll(/(?:)/g))).to.be.true;
+    });
+
     it('cast to type', () => {
-        expect(lo.toType(Boolean, 1)).to.be.a('boolean');
-        expect(lo.toType(Array, 1)).to.be.an.instanceof(Array);
-        expect(lo.toType(fx.TestArray, 1)).to.be.an.instanceof(Array);
-        expect(lo.toType(fx.TestError, 1)).to.be.an.instanceof(Error);
-        expect(lo.toType(String, 1)).to.be.a('string');
-        expect(lo.toType(BigInt, 1)).to.be.a('bigint');
-        expect(lo.toType(Map, [[1, 2]])).to.be.an.instanceof(Map);
-        expect(lo.toType(Set, [1, 2])).to.be.an.instanceof(Set);
+        expect(toType(Boolean, 1)).to.be.a('boolean');
+        expect(toType(Array, 1)).to.be.an.instanceof(Array);
+        expect(toType(fx.TestArray, 1)).to.be.an.instanceof(Array);
+        expect(toType(fx.TestError, 1)).to.be.an.instanceof(Error);
+        expect(toType(String, 1)).to.be.a('string');
+        expect(toType(BigInt, 1)).to.be.a('bigint');
+        expect(toType(Map, [[1, 2]])).to.be.an.instanceof(Map);
+        expect(toType(Set, [1, 2])).to.be.an.instanceof(Set);
     });
 
     it('isType', () => {
-        expect(lo.isError(new Error)).to.be.true;
-        expect(lo.isError(new fx.TestError)).to.be.true;
+        expect(isError(new Error)).to.be.true;
+        expect(isError(new fx.TestError)).to.be.true;
     });
 
     it('toArray', () => {
-        expect(lo.toArray(null)).to.eql([]);
-        expect(lo.toArray(undefined)).to.eql([]);
-        expect(lo.toArray(1,2,3)).to.eql([1,2,3]);
-        expect(lo.toArray([1,2,3])).to.eql([1,2,3]);
-        expect(lo.toArray('123')).to.eql(['1','2','3']);
-        expect(lo.toArray('123', '123')).to.eql(['1','2','3','1','2','3']);
-        expect(lo.toArray({1: 1})).to.eql([{1: 1}]);
-        expect(lo.toArray({1: 1}, {2: 2})).to.eql([{1: 1}, {2: 2}]);
-        expect(lo.toArray(true)).to.eql([true]);
-        expect(lo.toArray(fx.set)).to.eql([1,2,3]);
-        expect(lo.toArray(fx.map)).to.eql([[1,2],[3,4]]);
+        expect(toArray(null)).to.eql([]);
+        expect(toArray(undefined)).to.eql([]);
+        expect(toArray(1,2,3)).to.eql([1,2,3]);
+        expect(toArray([1,2,3])).to.eql([1,2,3]);
+        expect(toArray('123')).to.eql(['1','2','3']);
+        expect(toArray('123', '123')).to.eql(['1','2','3','1','2','3']);
+        expect(toArray({1: 1})).to.eql([{1: 1}]);
+        expect(toArray({1: 1}, {2: 2})).to.eql([{1: 1}, {2: 2}]);
+        expect(toArray(true)).to.eql([true]);
+        expect(toArray(fx.set)).to.eql([1,2,3]);
+        expect(toArray(fx.map)).to.eql([[1,2],[3,4]]);
     });
 
     it('toString', () => {
-        expect(lo.toString(null)).to.equal('');
-        expect(lo.toString(undefined)).to.equal('');
-        expect(lo.toString(NaN)).to.equal('NaN');
-        expect(lo.toString([1,2,3])).to.equal('1,2,3');
-        expect(lo.toString({1: 1, 2: 2})).to.equal('1,1,2,2');
-        expect(lo.toString(true)).to.equal('true');
-        expect(lo.toString(123)).to.equal('123');
-        expect(lo.toString(BigInt(123))).to.equal('123');
-        expect(lo.toString(fx.buff)).to.equal('test');
+        expect(toString(null)).to.equal('');
+        expect(toString(undefined)).to.equal('');
+        expect(toString(NaN)).to.equal('NaN');
+        expect(toString([1,2,3])).to.equal('1,2,3');
+        expect(toString({1: 1, 2: 2})).to.equal('1,1,2,2');
+        expect(toString(true)).to.equal('true');
+        expect(toString(123)).to.equal('123');
+        expect(toString(BigInt(123))).to.equal('123');
+        expect(toString(fx.buff)).to.equal('test');
     });
 
     it('of', () => {
